522c59a8d7243550d0603658b1b803827e76f084
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-15 16:14:10 +0100

    Create 1.1PurpleChallenge(1).js

---
+++ 1.1PurpleChallenge(1).js
@@ -0,0 +1,40 @@
+/*------------------------------*/ (Sprint4, JavaScript)
+/*------------------------------*/ (1st part of purple challenges)
+(2021.12.15)
+
+/*------------------------------*/ exo5/10
+var petDescription = name + " " + "-" + " " + breed +  ", " + age;
+console.log(petDescription);
+
+/*------------------------------*/ exo6/10
+var age = 3;
+var isPuppy = age < 2 === true;
+console.log(isPuppy);
+
+/*------------------------------*/ exo7/10
+var breed = "Labradaor";
+var requestedBreed = "Terrier";
+var match = breed === requestedBreed;
+console.log(match);
+
+/*------------------------------*/ exo8/10
+var breed = "Labrador"; 
+var avoidBreed = "Pitbull";
+var match = breed !== avoidBreed;
+console.log(match);
+
+/*------------------------------*/ exo9/10
+var match = false;
+message = "Please consider another breed.";
+console.log("Match: " + match);
+console.log(message);
+
+/*------------------------------*/ exo10/10
+var name = "Jessie";
+var lifeExpectancy = 14;
+var birthYear = 2018;
+var petYear = lifeExpectancy + birthYear;
+console.log(name + " is expected to live until " + petYear);
+// (output) Jessie is expected to live until 2032 
+
+
