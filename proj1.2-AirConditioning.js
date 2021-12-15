a4d361bfbd443d3efa9140cc5a655c53ec9949ca
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-15 18:50:49 +0100

    Update proj1.2-AirConditioning.js

--- proj1.2-AirConditioning.js
+++ proj1.2-AirConditioning.js
@@ -1,1 +1,11 @@
+/*-------------------*/
+/*-------------------*/
 
+var isWeekday = false;
+var acOn = true;
+
+console.log("Weekday");
+console.log(isWeekday);
+
+console.log("Air conditioning");
+console.log(acOn);
