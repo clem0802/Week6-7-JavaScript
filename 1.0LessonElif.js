e009be02e3d92ef083f7b20f44d62fcdbfc10f96
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-16 20:21:57 +0100

    Create 1.0LessonElif.js

---
+++ 1.0LessonElif.js
@@ -0,0 +1,21 @@
+/*-----------------------------*/
+/*-----------------------------*/ (BACK TICKS)
+
+const firstName = "Clémence";
+const lastName  = "TAN";
+const sentence = `My name is ${firstName} and my last name is ${lastName}.`;
+console.log(sentence);
+
+/*-----------------------------*/
+const numberOne = 10;
+const numberTwo = 49;
+const numberThree = 3;
+const division = numberTwo / numberThree;
+console.log(division.toFixed(4)); // 16.3333
+const remainder = numberOne % numberThree;
+console.log(remainder); // 1
+
+/*-----------------------------*/
+// how to find out the type of the variable
+const myNumber = "4";
+console.log(typeof myNumber); // string
