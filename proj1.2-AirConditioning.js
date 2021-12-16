87b476d187b0b2d24b58283bd00b6533603c2c94
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-16 19:26:33 +0100

    Update proj1.2-AirConditioning.js

--- proj1.2-AirConditioning.js
+++ proj1.2-AirConditioning.js
@@ -9,3 +9,8 @@ console.log(isWeekday);
 
 console.log("Air conditioning");
 console.log(acOn);
+
+isWeekday = false;
+acOn = !true;
+isWeekday = !false;
+var highTemperature = "max Air conditioning";
