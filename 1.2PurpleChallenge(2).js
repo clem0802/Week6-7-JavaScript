099bf9168a268ce43cd295c22721ffa79b3b2027
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-17 11:48:21 +0100

    Update 1.2PurpleChallenge(2).js

--- 1.2PurpleChallenge(2).js
+++ 1.2PurpleChallenge(2).js
@@ -2,6 +2,7 @@
 /*------------------------------*/ (2nd part of Dart Board - purple challenges)
 (2021.12.15)
 
+// exo: Comparing Numbers 2/4
 // Check if the value of characters is greater than or equal to 250 
 // and code a display statement to display the result of the comparison.
 
