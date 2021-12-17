3dcdce8e53483f03aa240dbc6f287b8850838573
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-17 12:17:18 +0100

    Update 1.2PurpleChallenge(2).js

--- 1.2PurpleChallenge(2).js
+++ 1.2PurpleChallenge(2).js
@@ -2,7 +2,9 @@
 /*------------------------------*/ (2nd part of Dart Board - purple challenges)
 (2021.12.15)
 
-// exo: Comparing Numbers 2/4
+
+/*------------------------------*/
+// exo: in "Comparing Numbers 2/4"
 // Check if the value of characters is greater than or equal to 250 
 // and code a display statement to display the result of the comparison.
 
@@ -11,3 +13,24 @@ console.log("Character limit reached:");
 console.log(characters >= 250); // (add this line)
 => Character limit reached:
 => false
+
+
+/*------------------------------*/
+// exo: in "Comparing Numbers 3/4"
+// Check if temperature is lower than 32 degrees 
+// and store the result of the comparison in a variable.
+var temperature = 30;
+var result = temperature < 32; // (add this line)
+
+
+/*------------------------------*/
+// exo: in "Comparing Numbers 4/4"
+// Check if age is greater than or equal to minAge and display the result in the console.
+var age = 19;
+var minAge = 16;
+console.log(age >= minAge); // (add this line)
+console.log("Old enough to drive:");
+
+
+
+
