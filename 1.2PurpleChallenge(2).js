5bb123161673b20555228907592914607a3fc56d
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-17 11:47:20 +0100

    Create 1.2PurpleChallenge(2).js

---
+++ 1.2PurpleChallenge(2).js
@@ -0,0 +1,12 @@
+/*------------------------------*/ (Sprint4, Types and Comparisons)
+/*------------------------------*/ (2nd part of Dart Board - purple challenges)
+(2021.12.15)
+
+// Check if the value of characters is greater than or equal to 250 
+// and code a display statement to display the result of the comparison.
+
+var characters = 134;
+console.log("Character limit reached:");
+console.log(characters >= 250); // (add this line)
+=> Character limit reached:
+=> false
