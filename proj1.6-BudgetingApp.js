eb6d588cec5347c7613f66efacd68ff7289bf701
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-17 16:52:31 +0100

    Update proj1.6-BudgetingApp.js

--- proj1.6-BudgetingApp.js
+++ proj1.6-BudgetingApp.js
@@ -1,1 +1,17 @@
 // Practice your string and number comparisons by coding a budgeting app.
+var budget = 3200;
+var savingPriority = "high";
+var rent = 1100;
+var utilities = 370;
+var newLaptop = 1200;
+var totalCosts = rent + utilities;
+var budgetLeft = budget - totalCosts;
+var buyLaptop = budgetLeft >= newLaptop;
+console.log(buyLaptop); // true
+console.log(savingPriority === "high"); // true
+
+budget = 2500;
+savingPriority = "low";
+rent = 800;
+var utilities = 200;
+console.log(buyLaptop); // true
